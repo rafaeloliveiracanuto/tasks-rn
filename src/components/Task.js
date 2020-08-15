ba63868175ec492821173ab03417594db396c643
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-15 20:57:21 -0300

    Create getCheckView function

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import Theme from '../Theme.js'
 
 export default props => {
 
     return (
         <View style={styles.container}>
-            <Text>{props.description}</Text>
-            <Text>{props.estimateAt + ''}</Text>
-            <Text>{props.doneAt + ''}</Text>
+            <View style={styles.checkContainer}>
+                {getCheckView(props.doneAt)}
+            </View>
+            <View>
+                <Text>{props.description}</Text>
+                <Text>{props.estimateAt + ''}</Text> 
+            </View>   
         </View>
     )
+}
+
+const getCheckView = (doneAt) => {
+    if (doneAt != null) {
+        return (
+            <View><Text>Done</Text></View>
+        )
+    } else {
+        return (
+            <View><Text>Pending</Text></View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -19,5 +37,8 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         alignItems: 'center',
         paddingVertical: 10,
+    },
+    checkContainer: {
+        width: '20%',
     }
 })
