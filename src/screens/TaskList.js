15470733384fc4bdd3b7bfe1aec40a3612798a0b
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-15 16:56:57 -0300

    Implement title and subtitle styles

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 
+import Theme from '../Theme'
 import todayImage from '../../assets/imgs/today.jpg'
 
 import moment from 'moment'
@@ -8,23 +9,24 @@ import 'moment/locale/en-gb'
 
 export default class TaskList extends Component {
     render() {
-        const today = moment().locale('en-gb').format('ddd, MMMM D[th]')
+        const today = moment().locale('en-gb').format('ddd, D MMMM')
 
-        return (
-
+        return (   
             <View style={styles.container}>
                 <ImageBackground style={styles.background} source={todayImage}>
-                    <View>
-                        <Text>Today</Text>
-                        <Text>{today}</Text>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Today</Text>
+                        <Text style={styles.subtitle}>{today}</Text>
                     </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <Text>TaskList</Text>
+                    <Text>Task 01</Text>
+                    <Text>Task 02</Text>
+                    <Text>Task 03</Text>
+                    <Text>Task 04</Text>
                 </View>
                 
             </View>
-
         )
     }
 }
@@ -39,4 +41,21 @@ const styles = StyleSheet.create ({
     taskList: {
         flex: 7
     },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontFamily: Theme.fontFamily,
+        fontSize: 50,
+        color: Theme.colors.secondary,
+        marginLeft: 20,
+    },
+    subtitle: {
+        fontFamily: Theme.fontFamily,
+        color: Theme.colors.secondary,
+        fontSize: 14,
+        marginLeft: 40,
+        marginBottom: 20
+    }
 })
