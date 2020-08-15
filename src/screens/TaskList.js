a75866d1bb64669604fc68e13709535aa041448c
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-15 16:19:51 -0300

    Fix date format

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -3,13 +3,21 @@ import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 
 import todayImage from '../../assets/imgs/today.jpg'
 
+import moment from 'moment'
+import 'moment/locale/en-gb'
+
 export default class TaskList extends Component {
     render() {
+        const today = moment().locale('en-gb').format('ddd, MMMM D[th]')
+
         return (
 
             <View style={styles.container}>
                 <ImageBackground style={styles.background} source={todayImage}>
-
+                    <View>
+                        <Text>Today</Text>
+                        <Text>{today}</Text>
+                    </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
                     <Text>TaskList</Text>
