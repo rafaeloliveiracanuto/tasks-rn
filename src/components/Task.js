20b521c58ce763f53b43587c63d18f2fa944c954
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-15 23:34:08 -0300

    Add TouchableWithoutFeedback

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TiuchableWithoutFeedback } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Theme from '../Theme.js'
 import moment from 'moment'
@@ -14,9 +14,11 @@ export default props => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.checkContainer}>
-                {getCheckView(props.doneAt)}
-            </View>
+            <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
+                <View style={styles.checkContainer}>
+                    {getCheckView(props.doneAt)}
+                </View>
+            </TouchableWithoutFeedback>
             <View>
                 <Text style={[styles.description, doneOrNotStyle]}>{props.description}</Text>
                 <Text style={styles.date}>{formattedDate}</Text> 
