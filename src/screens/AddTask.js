360d1ad888536f018b72e3fb42271b08e6fd4a8b
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-16 22:17:33 -0300

    Add AddTask class

---
+++ src/screens/AddTask.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback }  from 'react-native'
+
+export default class AddTask extends Component {
+
+    render () {
+        
+    }
+}
+
