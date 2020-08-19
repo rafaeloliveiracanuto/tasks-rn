c460438e90f4210c22e6b89b3bf65aea234bd0b0
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-18 23:20:31 -0300

    Add calls

--- src/components/Task.js
+++ src/components/Task.js
@@ -20,7 +20,8 @@ export default props => {
 
     const getRightContent = () => {
         return (
-            <TouchableOpacity style={styles.right}>
+            <TouchableOpacity style={styles.right}
+                onPress={() => props.onDelete && props.onDelete(props.id)}>
                 <Icon name='trash' size={30} color='#FFF' />
             </TouchableOpacity>
         )
@@ -37,7 +38,8 @@ export default props => {
     }
 
     return (
-        <Swipeable renderRightActions={getRightContent} renderLeftActions={getLeftContent}>
+        <Swipeable renderRightActions={getRightContent} renderLeftActions={getLeftContent}
+            onSwipeableLeftOpen={() => props.onDelete && props.onDelete(props.id)}>
             <View style={styles.container}>
                 <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
                     <View style={styles.checkContainer}>
