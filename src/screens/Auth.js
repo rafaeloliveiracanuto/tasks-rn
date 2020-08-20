5f72a2d5e03615ef4af38d5fc3981a651a86a20f
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-19 23:09:39 -0300

    Add more styles

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react'
 import { ImageBackground, Text, StyleSheet }from 'react-native'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
+import Theme from '../Theme'
 
 export default class Auth extends Component {
     render() {
         return (
-            <ImageBackground source={}>
+            <ImageBackground style={styles.background}
+                source={backgroundImage}>
                 <Text style={styles.title}>Tasks</Text>
             </ImageBackground>
         )
@@ -15,5 +17,16 @@ export default class Auth extends Component {
 }
 
 const styles = StyleSheet.create({
-    
+    background: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontFamily: Theme.fontFamily,
+        color: Theme.colors.secondary,
+        fontSize: 70,
+        marginBottom: 10,
+    },
 })
