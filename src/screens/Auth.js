134949f4fa808d5e801d45ff5ca5143e9cbceed8
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-19 23:02:53 -0300

    Create Auth class

---
+++ src/screens/Auth.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { ImageBackground, Text, StyleSheet }from 'react-native'
+
+export default class Auth extends Component {
+    render() {
+        return (
+            
+        )
+    }
+}
