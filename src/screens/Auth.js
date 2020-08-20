eb345767b389013f22da77e57515cecff3564115
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-19 23:05:24 -0300

    Create styles

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,10 +1,19 @@
+/* eslint-disable */
 import React, { Component } from 'react'
 import { ImageBackground, Text, StyleSheet }from 'react-native'
+
+import backgroundImage from '../../assets/imgs/login.jpg'
 
 export default class Auth extends Component {
     render() {
         return (
-            
+            <ImageBackground source={}>
+                <Text style={styles.title}>Tasks</Text>
+            </ImageBackground>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    
+})
