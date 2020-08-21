1a797f545417e2c4f6edf62d4a69e5752e36937a
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-20 21:33:54 -0300

    Add properties

--- src/components/AuthInput.js
+++ src/components/AuthInput.js
@@ -1,16 +1,19 @@
+/* eslint-disable */
 import React from 'react'
 import { View, TextInput, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default props => {
     return (
-        <View style={[]}>
-            <Icon />
-            <TextInput />
+        <View style={[styles.container, props.style]}>
+            <Icon name={props.icon} size={20} style={styles.icon} />
+            <TextInput {...props} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+
+    },
 })
