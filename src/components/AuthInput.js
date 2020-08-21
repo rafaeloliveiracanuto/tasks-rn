298f06307e57ad910ef87578a38ee4baad9ceb9b
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-20 21:29:53 -0300

    Create styles

--- src/components/AuthInput.js
+++ src/components/AuthInput.js
@@ -3,5 +3,14 @@ import { View, TextInput, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default props => {
-    
+    return (
+        <View style={[]}>
+            <Icon />
+            <TextInput />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    
+})
