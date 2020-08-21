44069832dcd22bea1241ad5438e1c1d58f44857f
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-20 21:11:14 -0300

    Import all

---
+++ src/Navigator.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+
+import TaskList from './screens/TaskList'
+import Auth from './screens/Auth'
+
