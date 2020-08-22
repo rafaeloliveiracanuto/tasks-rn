300fbd9e50ee86f67f77201f35cdea33bc204984
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-21 22:19:26 -0300

    Add showSuccess function

--- src/common.js
+++ src/common.js
@@ -4,6 +4,10 @@ import { Alert, Platform } from 'react-native'
 const server = Platform.OS === 'ios' 
     ? 'http://localhost:3000' : 'http://10.0.2.2:3000'
 
-function showError(err) {
-    Alert.alert('Ops! A problem has ocurred!', `Message: ${err}`)
+function showError(error) {
+    Alert.alert('Ops! A problem has ocurred!', `Message: ${error}`)
+}
+
+function showSuccess(message) {
+    Alert.alert('Success!', message)
 }
