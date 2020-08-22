e1c6855e20531d2887fb24cb553b3a947b56f7ae
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-21 22:18:08 -0300

    Add showError function

---
+++ src/common.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { Alert, Platform } from 'react-native'
+
+const server = Platform.OS === 'ios' 
+    ? 'http://localhost:3000' : 'http://10.0.2.2:3000'
+
+function showError(err) {
+    Alert.alert('Ops! A problem has ocurred!', `Message: ${err}`)
+}
