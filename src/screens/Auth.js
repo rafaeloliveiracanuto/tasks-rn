b82bd0e6da27179b0d5c36d3add2aca0f746ceb7
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-21 22:27:14 -0300

    Finish function

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { ImageBackground, Text, StyleSheet, Alert,
     View, TouchableOpacity } from 'react-native'
 
+import axios from 'axios'
 import backgroundImage from '../../assets/imgs/login.jpg'
 import Theme from '../Theme'
 import AuthInput from '../components/AuthInput'
@@ -27,8 +28,20 @@ export default class Auth extends Component {
         }
     }
 
-    signUp = () => {
-        
+    signUp = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            })
+
+            showSuccess('User successfully registered!')
+            this.setState({ stageNew: false })
+        } catch(e) {
+            showError(e)
+        }
     }
 
     render() {
